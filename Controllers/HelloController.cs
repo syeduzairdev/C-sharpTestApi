217a353c8c0f7f459e18608bf587643597c50783
SyedUzair42101 <uzairhassan206@gmail.com>
2022-09-03 02:02:03 +0500

    get and post test apis

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public  IActionResult Get()
+        {
+            return Ok("Hello Myself Uzair Its My first Web Api");
+        }
+        [HttpPost]
+        public IActionResult Post(JsonOptions payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
